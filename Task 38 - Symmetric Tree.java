3de8e79a2e9a479d830872a25c0c707cba6792f0
Prathesh7 <vtu27431@veltech.edu.in>
2026-09-10 09:50:52 +0530

    Add files via upload

---
+++ Task 38 - Symmetric Tree.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isMirror(left.left, right.right)
+                && isMirror(left.right, right.left);
+    }
+}
+
+OUTPUT:
+Accepted
+
+Runtime: 0 ms
+
+Case 1
+Case 2
+
+Input:
+root = [1, 2, 2, 3, 4, 4, 3]
+
+Output:
+true
+
+Expected:
+true
